7dcd9c4d46945fa961f0fa6f4516ef1ee4fc3013
Hyunmin Choi <hyunmin.choi@deliveryhero.com>
2015-10-22 19:31:21 +0200

    Get rid of unnecessary part due to book

--- web/hoodpub/models.py
+++ web/hoodpub/models.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
@@ -18,29 +17,3 @@ def create_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
 
 post_save.connect(create_user_profile, sender=User)
-
-
-class Book(models.Model):
-    category = models.CharField(max_length=168)
-    sale_yn = models.CharField(max_length=16)
-    barcode = models.CharField(max_length=168)
-    isbn = models.CharField(max_length=168)
-    isbn13 = models.CharField(max_length=168)
-    cover_s_url = models.CharField(max_length=512)
-    author = models.CharField(max_length=168)
-    author_t = models.CharField(max_length=168)
-    sale_price = models.CharField(max_length=168)
-    title = models.CharField(max_length=168)
-    translator = models.CharField(max_length=168)
-    link = models.CharField(max_length=168)
-    etc_author = models.CharField(max_length=128)
-    pub_nm = models.CharField(max_length=128)
-    list_price = models.CharField(max_length=168)
-    ebook_barcode = models.CharField(max_length=168)
-    cover_l_url = models.CharField(max_length=512)
-    pub_date = models.DateTimeField(default=datetime.now, blank=True)
-    status_des = models.CharField(max_length=168)
-    description = models.TextField()
-
-    def __unicode__(self):
-        return u'%d %s' % (self.pk, self.title)
