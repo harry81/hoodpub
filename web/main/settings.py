be26c771037b684e3b87fd3b2d54b0c9636c1a6b
Hyunmin Choi <hyunmin.choi@deliveryhero.com>
2015-10-23 19:15:42 +0200

    Added auth models

--- web/main/settings.py
+++ web/main/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = (
     'djcelery',
     'book',
     'hoodpub',
+    'hoodpub_auth',
 )
 
 MIDDLEWARE_CLASSES = (
--- web/main/urls.py
+++ web/main/urls.py
@@ -12,6 +12,7 @@ router.register(r'api-book', book_views.BookAPIView, base_name="api-book")
 urlpatterns = patterns('',
      url(r'^$', hoodpub_views.index),
      url(r'^', include(router.urls)),
+     url(r'^auth/', include('hoodpub_auth.urls')),
      url(r'^admin/', include(admin.site.urls)),
      url(r'^docs/', include('rest_framework_swagger.urls')),
 )
