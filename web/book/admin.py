73f9003ca408baae6699cc7ee7f5649cb6ca8f3e
Hyunmin Choi <hyunmin.choi@deliveryhero.com>
2015-10-24 15:12:26 +0200

    Added admin for books

--- web/book/admin.py
+++ web/book/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Books
 
-# Register your models here.
+
+class BooksAdmin(admin.ModelAdmin):
+
+    def show_cover_s_url(self):
+        return u"<img src='%s'>" % self.cover_s_url
+
+    show_cover_s_url.allow_tags = True
+
+    def view_link_url(self):
+        return u"<a href='%s'>%s</a>" % (self.link, self.barcode)
+    view_link_url.allow_tags = True
+
+    list_display = ('isbn', show_cover_s_url, 'title', 'author', 'author_t',
+                    view_link_url, 'pub_nm', 'sale_price')
+    list_display_links = ('title',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'link', 'pub_nm', 'sale_price')
+            }), ('Advanced options', {
+                'fields': (
+                    ('author', 'author_t'),
+                    ('isbn', 'isbn13'),
+                    ('description'),
+                    )
+                }),
+        )
+
+
+admin.site.register(Books, BooksAdmin)
