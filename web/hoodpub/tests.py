b51fcb67c4e043c7e7704739198d96c409e55949
Hyunmin Choi <hyunmin.choi@deliveryhero.com>
2015-10-25 13:48:33 +0100

    added facebook read action
    check is necessary to see if it works on facebook

--- web/hoodpub/tests.py
+++ web/hoodpub/tests.py
@@ -4,7 +4,9 @@ from django.test import TestCase
 
 class HoodpubTestCase(TestCase):
 
+    fixtures = ['book']
+
     def test_facebook_action_read(self):
         res = self.client.post('/api-hoodpub/read/',
-                               {'search': u'삼국지'}, format='json')
+                               {'isbn': u'8995843543'}, format='json')
         self.assertIn('id', res.data.text)
--- web/hoodpub/utils.py
+++ web/hoodpub/utils.py
@@ -2,9 +2,13 @@
 
 import requests
 from urlparse import urljoin
+from django.conf import settings
+from book.models import Book
 
 
 def facebook_action_read(request):
+    isbn = request.POST.get('isbn')
+    book = Book.objects.get(isbn=isbn)
 
     url_dict = {
         'access_token': 'CAAKaRbf67W8BAPwbVrLJ7twVaRQbxzF8BnKFBhytyUZBJX9e8gsHMYFU77O2bJWN1ZBBvZBaAu4euAewzp422cIk4jRIH7facChyBFCMV98AWlMYz3uXkxSGjZCbN5LCFhaXglRZBLNyqnfZAdPLtZCBXJE0VO0rV7svn3lPVOWPnLsacWemmjF2ICxKGA7OnEZD',
@@ -15,19 +19,20 @@ def facebook_action_read(request):
         "object":
         {
             "books:author": "http://samples.ogp.me/344562145628374",
-            "books:isbn": "0553380958",
-            "fb:app_id": 732574183517551,
-            "og:description": "In reality, Hiro Protagonist delivers pizza for Uncle Enzo\u2019s CosoNostra Pizza Inc., but in the Metaverse he\u2019s a warrior prince. Plunging headlong into the enigma of a new computer virus that\u2019s striking down hackers everywhere, he races along the neon-lit streets on a search-and-destroy mission for the shadowy virtual villain threatening to bring about infocalypse. Snow Crash is a mind-altering romp through a future America so bizarre, so outrageous\u2026you\u2019ll recognize it immediately.",
-            "og:image": "http://en.wikipedia.org/wiki/File:Snowcrash.jpg",
+            "books:isbn": book.isbn,
+            "fb:app_id": settings.SOCIAL_AUTH_FACEBOOK_KEY,
+            "og:description": book.description,
+            "og:image": book.cover_s_url,
             "og:type": "books.book",
             "og:title": "Snow Crash",
-            "og:url": "https://www.facebook.com/pages/Anne-Frank-The-Young-Writer-Who-Told-the-World-Her-Story/477596482277558?rf=316285781810761"
+            "og:url": "http://localhost:8000/#/book_detail?id=8997969145",
         }
     }
 
     url = 'https://graph.facebook.com/'
     action = 'me/objects/books.book'
     url = urljoin(url, action)
+    import ipdb; ipdb.set_trace()
     res = requests.post(url, params=url_dict, json=data_dict)
 
     return res
