c9d88a406d77af5b66f15e570cb0d99e4485a57f
Hyunmin Choi <hyunmin.choi@deliveryhero.com>
2015-11-01 18:02:03 +0100

    added newrelic ini

--- web/main/wsgi.py
+++ web/main/wsgi.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import newrelic.agent
+newrelic.agent.initialize('newrelic.ini')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+application = newrelic.agent.wsgi_application()(application)
