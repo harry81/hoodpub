142dd9236ad83e8c27b316a908153601b5d6c3cf
Hyunmin Choi <hyunmin.choi@deliveryhero.com>
2015-11-03 20:12:33 +0100

    Customized admin for UserSocialAuth

--- web/hoodpub/admin.py
+++ web/hoodpub/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+
+from social.apps.django_app.default.models import UserSocialAuth
+from social.apps.django_app.default.admin import UserSocialAuthOption
+
 from .models import UserProfile, Read
 
 
@@ -21,3 +25,16 @@ class ReadAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Read, ReadAdmin)
+
+
+class CustomUserSocialAuthOption(UserSocialAuthOption):
+    def uid_picture(self, instance):
+        return '<a href="https://www.facebook.com/{0}" target="_new">\
+        <img src="https://graph.facebook.com/{0}/picture/"></a>'.format(
+            instance.uid)
+    uid_picture.allow_tags = True
+
+    list_display = ('id', 'uid_picture', 'user', 'provider', 'uid')
+
+admin.site.unregister(UserSocialAuth)
+admin.site.register(UserSocialAuth, CustomUserSocialAuthOption)
