06ec274ccefef1abd9c43f9ddc672f962fa5a1cb
Hyunmin Choi <hyunmin.choi@deliveryhero.com>
2015-11-12 20:11:44 +0100

    fix pep8

--- web/book/management/commands/trim_isbn.py
+++ web/book/management/commands/trim_isbn.py
@@ -12,8 +12,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for book in Book.objects.all():
             if len(book.isbn) > 11:
-                cnt = book.read_set.all().count()
-                if cnt > 1:
+                if book.read_set.all().count() > 1:
                     old_book = book
                     isbn = strip_tags(self.parser.unescape(book.isbn))
                     try:
@@ -22,23 +21,21 @@ class Command(BaseCommand):
                         continue
 
                     for read in old_book.read_set.all():
-                        self.stdout.write("OLD %14s - %14s\n" %
-                                          (book.isbn, read)
-                                          , ending='')
+                        self.stdout.write("OLD %14s-%14s\n" % (
+                            book.isbn, read), ending='')
 
                     for read in new_book.read_set.all():
-                        self.stdout.write("NEW %14s - %14s %d\n" % (book.isbn, read, read.id)
-                                          ,ending='')
-
+                        self.stdout.write("NEW %14s - %14s %d\n" % (
+                            book.isbn, read, read.id), ending='')
 
                     for read in old_book.read_set.all():
                         read.book = new_book
                         read.save()
 
                     for read in old_book.read_set.all():
-                        self.stdout.write("OLD %14s - %14s %d\n" % (book.isbn, read, read.id)
-                                          ,ending='')
+                        self.stdout.write("OLD %14s - %14s %d\n" % (
+                            book.isbn, read, read.id), ending='')
 
                     for read in new_book.read_set.all():
-                        self.stdout.write("NEW %14s - %14s %d\n" % (book.isbn, read, read.id)
-                                          ,ending='')
+                        self.stdout.write("NEW %14s - %14s %d\n" % (
+                            book.isbn, read, read.id), ending='')
