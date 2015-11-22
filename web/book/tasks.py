7cf03cf5abfdc7b6b894597e426e47161ee6fe78
Hyunmin Choi <hyunmin.choi@deliveryhero.com>
2015-11-22 18:23:40 +0100

    Run book search task after 5 secs

--- web/book/tasks.py
+++ web/book/tasks.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
+import time
 from celery import shared_task
 from .utils import search_via_book_api
 
 
 @shared_task
 def async_search_via_book_api(title):
+    time.sleep(5)
     return search_via_book_api(title=title)
